7a5e0a1c15fbf8631971810c3168db4bc43eaa04
Sleepin'_Fang <57334019+sleepin-fang@users.noreply.github.com>
2023-05-27 15:09:59 +0800

    AQI site

---
+++ aqi/script.js
@@ -0,0 +1,29 @@
+function darkMode() {
+    const bodyElement = document.body;
+    const contentElement = document.getElementsByClassName('content-section');
+    const tableElement = document.getElementById('content-table');
+        
+    if (document.getElementById('theme').checked) {
+        bodyElement.classList.add('dark-theme');
+        for (var i = 0; i < contentElement.length; i++) {
+            contentElement[i].classList.add('dark-theme');
+        }
+        tableElement.classList.add('dark-theme');
+    
+    }
+
+    else {
+        bodyElement.classList.remove('dark-theme');
+        for (var i = 0; i < contentElement.length; i++) {
+            contentElement[i].classList.remove('dark-theme');
+        }
+        tableElement.classList.remove('dark-theme');
+    }
+
+}
+
+
+function setLanguage(input) {
+    window.location.href = "AQI_" + input + ".html";
+    //console.log(input);
+}
